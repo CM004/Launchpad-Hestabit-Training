01fc210c393d6feb9620ff4154a428d3c53335e6
Chandramohan <Senchandramohan2018@gmail.com>
2025-12-15 16:16:11 +0530

    added mongodb database

--- WEEK5/server/index.js
+++ WEEK5/server/index.js
@@ -1,7 +1,12 @@
+const mongoose = require("../mongodb/mongo");
 const express = require("express");
 const app = express();
 app.get("/",(req,res)=>{
-    res.json({message:"hello docker"});
+    res.json({
+        message:"hello docker",
+        status: "ok",
+        db: mongoose.connection.readyState === 1 ? "connected" : "disconnected" 
+    });
 });
 
 app.listen(9000,()=>console.log("server running at port 9000"));
