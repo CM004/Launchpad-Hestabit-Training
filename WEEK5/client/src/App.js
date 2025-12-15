ea1c04c7a545ee894b6413c394d2df43faa8e6b7
Chandramohan <Senchandramohan2018@gmail.com>
2025-12-15 16:11:59 +0530

    added dockerfile for client folder

--- WEEK5/client/src/App.js
+++ WEEK5/client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1> WEEK 5 Docker Multi Container App</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
