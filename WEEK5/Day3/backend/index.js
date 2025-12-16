2a9b2c85da6deb5afe1384feac5a15ffe8cb2613
Chandramohan <Senchandramohan2018@gmail.com>
2025-12-16 12:52:06 +0530

    made server on port 3000 with endpoint 'api'

---
+++ WEEK5/Day3/backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import os from "os";
+
+const app = express();
+const PORT = 3000;
+const hostname = os.hostname();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Server is running", dockerContainer: hostname });
+});
+
+app.listen(PORT, () => console.log(`Backend running on ${PORT} - ${hostname}`));
